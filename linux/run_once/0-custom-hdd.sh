2990f0be6752e9df7d0548b553fa15128a48399c
Ching Chow <cchow@jungroup.com>
2017-07-14 16:24:48 -0400

    Mount over homedir

--- linux/run_once/0-custom-hdd.sh
+++ linux/run_once/0-custom-hdd.sh
@@ -3,14 +3,14 @@ set -e
 
 
 DEVICE="/dev/sdc"
-MOUNT="${HOME}/extra-hdd"
-LABEL="$(basename "${MOUNT}")"
+LABEL="custom-hdd"
+MOUNT="${HOME}"
 
 if sudo fdisk -l "${DEVICE}" | grep -q "Disklabel"
 then
     echo "${DEVICE} is ready"
 else
-    echo "${DEVICE} is being prepared..."
+    echo "Preparing ${DEVICE}..."
 
     {
         echo g;  # new GPT partition table
@@ -25,8 +25,13 @@ else
     sudo e2label "${DEVICE}1" "${LABEL}"  # label partition 1
 
     echo "LABEL=${LABEL} ${MOUNT} ext4 defaults 0 0" | sudo tee -a /etc/fstab
-    mkdir "${MOUNT}"
+
+    sudo rsync -av "${MOUNT}/" /tmp_home
     sudo mount -a
+
+    sudo rsync -av /tmp_home/ "${MOUNT}"
+    sudo rm -r /tmp_home
+    sudo chown -R "${USER}:${USER}" "${MOUNT}"
 
     echo "${DEVICE} is ready"
 fi
