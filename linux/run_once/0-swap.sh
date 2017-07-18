bfdbc4e5b6c8356e828b3c9e0e515a26a9ca339e
Ching Chow <cchow@jungroup.com>
2017-07-18 14:16:30 -0400

    Move swap file onto custom hdd

--- linux/run_once/0-swap.sh
+++ linux/run_once/0-swap.sh
@@ -2,10 +2,12 @@
 set -e
 
 
-sudo dd if=/dev/zero of=/swap bs=1024 count=6291456  # kilobytes
-sudo chmod 600 /swap
-sudo mkswap /swap
-sudo swapon /swap
+SWAP="${HOME}/.swap"
 
-echo "/swap none swap sw 0 0" | sudo tee -a /etc/fstab
+sudo dd if="/dev/zero" of="${SWAP}" bs=1024 count=6291456  # kilobytes
+sudo chmod 600 "${SWAP}"
+sudo mkswap "${SWAP}"
+sudo swapon "${SWAP}"
+
+echo "${SWAP} none swap sw 0 0" | sudo tee -a /etc/fstab
 sudo mount -a
