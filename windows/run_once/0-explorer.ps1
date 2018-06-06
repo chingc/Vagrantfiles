8c7cc07d86ca20320d1b27e887df4a1b23be57c5
Ching Chow <ching.chow.sc@gmail.com>
2018-06-06 18:50:26 -0400

    Combine desktop preferences

--- windows/run_once/0-explorer.ps1
+++
@@ -1,5 +0,0 @@
-$key = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-Set-ItemProperty $key Hidden 1
-Set-ItemProperty $key HideFileExt 0
-Set-ItemProperty $key ShowSuperHidden 1
-Stop-Process -processname explorer
--- windows/run_once/0-wallpaper.ps1
+++
@@ -1,3 +0,0 @@
-$key = "HKCU:\Control Panel\Desktop"
-Set-ItemProperty $key "TileWallpaper" 0
-Set-ItemProperty $key "WallpaperStyle" 0
